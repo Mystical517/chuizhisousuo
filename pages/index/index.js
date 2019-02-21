2c04f5f2ce7c74a4f7637eeaa6bdc804544af1e0
陈ls <chenjl18@lzu.edu.cn>
2019-02-22 05:22:39 +0800

    垂直搜索v1.0.2添加电影搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,65 @@
-Page({
+Page(
+  {
+    data:{
+      music_name:'',
+      list_data:[]
+    },
+    setmusicname:function(e){
+      this.setData({
+        music_name: e.detail.value
+      })
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
-  },
-
-  /**
+    setmoviename: function (e) {
+      this.setData({
+        movie_name: e.detail.value
+      })
+    }
+    ,
+    music_search: function () {
+      var str_data=''
+      var that = this//不要漏了这句，很重要
+      wx.request({
+        url: 'http://101.132.132.129:5000/music/'+that.data.music_name,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          var app=getApp()
+          app.globalData.music_list_data=res.data
+          wx.navigateTo({
+            url: 'music_search'
+          })
+        }
+      })
+    }
+    ,
+    movie_search: function () {
+      var str_data = ''
+      var that = this//不要漏了这句，很重要
+      wx.request({
+        url: 'http://101.132.132.129:5000/movie/' + that.data.movie_name,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          var app = getApp()
+          app.globalData.movie_list_data = res.data
+          console.log(res.data)
+          wx.navigateTo({
+            url: 'movie_search'
+          })
+        }
+      })
+    }
+  ,/**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     
   },
 
+   
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
